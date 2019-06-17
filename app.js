34e660c50a8d80f0b585975f8569c06bbfde4759
Roshan Adhikari <nahsorad@gmail.com>
2019-06-17 20:23:09 +0545

    Add environment variable PORT

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+// Imports
 const app = require('express')();
+const dotenv = require('dotenv').config();
 const fs = require('fs');
 const request = require('request');
 
-const PORT = 5000;
+// Environment variable
+const PORT = process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
+// GET method routes
+app.get('/api', (req, res) => {
+    res.send("API backend for Movie Digest");
+});
+
+app.get('/api', (req, res) => {
+    res.send("API backend for Movie Digest");
 });
 
 app.listen(PORT, () => {
