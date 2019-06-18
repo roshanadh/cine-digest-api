2f0ec44339a808ce5084bb5adc56ff32b4637ee3
Roshan Adhikari <nahsorad@gmail.com>
2019-06-18 21:06:22 +0545

    Employ response function

--- app.js
+++ app.js
@@ -11,9 +11,49 @@ const OMDB_KEY = process.env.OMDB_KEY;
 // Request URLs
 const apiUrl = 'http://www.omdbapi.com/';
 const apiKey = "&apikey=" + OMDB_KEY;
-//  + movieTitle + '&apikey=' + OMDB_KEY;
-let finalSearchUrl;
 
+// Final search URL
+let finalSearchUrl;
+let responseStatus;
+
+function respond(fetchResponse){
+    let response;
+    
+    if(fetchResponse.Response == "False"){
+        response = {"Message": "False"};
+    }
+    else{
+        let message = "True";
+        let movieTitle = fetchResponse.Title;
+        let movieYear = fetchResponse.Year;
+        let movieRated = fetchResponse.Rated;
+        let movieRuntime = fetchResponse.Runtime;
+        let movieGenre = fetchResponse.Genre;
+        let movieDirector = fetchResponse.Director;
+        let movieActors = fetchResponse.Actors;
+        let moviePlot = fetchResponse.Plot;
+        let movieLanguage = fetchResponse.Language;
+        let movieImdbRating = fetchResponse.imdbRating; 
+        let posterPath = fetchResponse.Poster;
+
+        response = {
+            "Message": message,
+            "Title": movieTitle,
+            "Year": movieYear,
+            "Rated": movieRated,
+            "Runtime": movieRuntime,
+            "Genre": movieGenre,
+            "Director": movieDirector,
+            "Actors": movieActors,
+            "Plot": moviePlot,
+            "Language": movieLanguage,
+            "imdbRating": movieImdbRating,
+            "Poster": posterPath
+        }
+    }
+    console.log(response);
+    return response;
+}
 // GET method routes
 // Landing route
 app.get('/api/v1', (req, res) => {
@@ -35,43 +75,15 @@ app.get('/api/v1/searchm/:query/', (req, res) => {
         else{
             console.log(resp);
 
-           // response.body is a JSON object
-           fetchResponse = JSON.parse(resp.body); 
-           if(fetchResponse.Response == "False"){
-               res.status(404).send({"message": "False"});
-           }
-           else{
-               let message = "True";
-               let movieTitle = fetchResponse.Title;
-               let movieYear = fetchResponse.Year;
-               let movieRated = fetchResponse.Rated;
-               let movieRuntime = fetchResponse.Runtime;
-               let movieGenre = fetchResponse.Genre;
-               let movieDirector = fetchResponse.Director;
-               let movieActors = fetchResponse.Actors;
-               let moviePlot = fetchResponse.Plot;
-               let movieLanguage = fetchResponse.Language;
-               let movieImdbRating = fetchResponse.imdbRating; 
-               let posterPath = fetchResponse.Poster;
-
-               let sendResponse = {
-                   "Message": message,
-                   "Title": movieTitle,
-                   "Year": movieYear,
-                   "Rated": movieRated,
-                   "Runtime": movieRuntime,
-                   "Genre": movieGenre,
-                   "Director": movieDirector,
-                   "Actors": movieActors,
-                   "Plot": moviePlot,
-                   "Language": movieLanguage,
-                   "imdbRating": movieImdbRating,
-                   "Poster": posterPath
-               }
+            // response.body is a JSON object
+            fetchResponse = JSON.parse(resp.body); 
            
-               console.log(sendResponse);
-               res.status(200).send(sendResponse);
-           }
+            let response = respond(fetchResponse);
+            if(response.Message == "True")
+                res.status(200);
+            else
+                res.status(404);
+            res.send(response);            
         }
     });
 
@@ -93,41 +105,13 @@ app.get('/api/v1/searchm/:query/:year', (req, res) => {
 
             // response.body is a JSON object
             fetchResponse = JSON.parse(resp.body); 
-            if(fetchResponse.Response == "False"){
-                res.status(404).send({"message": "False"});
-            }
-            else{
-                let message = "True";
-                let movieTitle = fetchResponse.Title;
-                let movieYear = fetchResponse.Year;
-                let movieRated = fetchResponse.Rated;
-                let movieRuntime = fetchResponse.Runtime;
-                let movieGenre = fetchResponse.Genre;
-                let movieDirector = fetchResponse.Director;
-                let movieActors = fetchResponse.Actors;
-                let moviePlot = fetchResponse.Plot;
-                let movieLanguage = fetchResponse.Language;
-                let movieImdbRating = fetchResponse.imdbRating; 
-                let posterPath = fetchResponse.Poster;
-
-                let sendResponse = {
-                    "Message": message,
-                    "Title": movieTitle,
-                    "Year": movieYear,
-                    "Rated": movieRated,
-                    "Runtime": movieRuntime,
-                    "Genre": movieGenre,
-                    "Director": movieDirector,
-                    "Actors": movieActors,
-                    "Plot": moviePlot,
-                    "Language": movieLanguage,
-                    "imdbRating": movieImdbRating,
-                    "Poster": posterPath
-                }
-            
-                console.log(sendResponse);
-                res.status(200).send(sendResponse);
-            }
+           
+            let response = respond(fetchResponse);
+            if(response.Message == "True")
+                res.status(200);
+            else
+                res.status(404);
+            res.send(response);   
         }
     });
 
