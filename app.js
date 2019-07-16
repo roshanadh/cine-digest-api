d52344037bbf4b3606d74921ef7f8939c5d8d914
Roshan Adhikari <nahsorad@gmail.com>
2019-07-16 18:25:14 +0545

    Set indentation formatting for JSON response

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = require('express')();
 const router = require('./routes/index.js');
 const {PORT} = require('./utility.js');
 
+app.set('json spaces', 2);
+
 app.use(router);
 
 app.listen(PORT, () => {
