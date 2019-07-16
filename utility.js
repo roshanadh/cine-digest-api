8c1ced5138de6161a749eab7ef4e998e88f6d10d
Roshan Adhikari <nahsorad@gmail.com>
2019-07-16 20:55:43 +0545

    Export PRIMARY_RELEASE_YEAR_STRING

--- utility.js
+++ utility.js
@@ -1,18 +1,21 @@
+/* eslint-disable linebreak-style */
 require('dotenv').config();
 
 // Environment variables
 const PORT = process.env.PORT || 3000;
-const OMDB_KEY = process.env.OMDB_KEY;
-const TMDB_KEY = process.env.TMDB_KEY;
-const BASE_URL = process.env.BASE_URL;
-const API_KEY_STRING = process.env.API_KEY_STRING;
-const QUERY_STRING = process.env.QUERY_STRING;
+const { OMDB_KEY } = process.env;
+const { TMDB_KEY } = process.env;
+const { BASE_URL } = process.env;
+const { API_KEY_STRING } = process.env;
+const { QUERY_STRING } = process.env;
+const { PRIMARY_RELEASE_YEAR_STRING } = process.env;
 
 module.exports = {
-    PORT,
-    OMDB_KEY,
-    TMDB_KEY,
-    BASE_URL,
-    API_KEY_STRING,
-    QUERY_STRING,
-}
+  PORT,
+  OMDB_KEY,
+  TMDB_KEY,
+  BASE_URL,
+  API_KEY_STRING,
+  QUERY_STRING,
+  PRIMARY_RELEASE_YEAR_STRING,
+};
