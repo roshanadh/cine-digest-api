45c8b4f71ad52c708e7a5a173416a0885e12aa09
Roshan Adhikari <nahsorad@gmail.com>
2019-07-16 18:24:07 +0545

    Export {key, URL, strings} from utility.js

--- utility.js
+++ utility.js
@@ -3,6 +3,16 @@ require('dotenv').config();
 // Environment variables
 const PORT = process.env.PORT || 3000;
 const OMDB_KEY = process.env.OMDB_KEY;
+const TMDB_KEY = process.env.TMDB_KEY;
+const BASE_URL = process.env.BASE_URL;
+const API_KEY_STRING = process.env.API_KEY_STRING;
+const QUERY_STRING = process.env.QUERY_STRING;
 
-module.exports.PORT = PORT;
-module.exports.OMDB_KEY = OMDB_KEY;
+module.exports = {
+    PORT: PORT,
+    OMDB_KEY: OMDB_KEY,
+    TMDB_KEY: TMDB_KEY,
+    BASE_URL: BASE_URL,
+    API_KEY_STRING: API_KEY_STRING,
+    QUERY_STRING: QUERY_STRING,
+}
