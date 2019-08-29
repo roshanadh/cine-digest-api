bdb1c10177f7a6ffbea7a9ef1f336445514f5b3a
Roshan Adhikari <nahsorad@gmail.com>
2019-08-29 16:21:05 +0545

    Export MySQL connection from db/index.js

--- db/index.js
+++ db/index.js
@@ -9,57 +9,19 @@ const {
     DBPASSWORD,
 } = require('../utility.js');
 
-
-class DB {
-    static getConnection() {
-        const connection = mysql.createConnection({
-            host: DBHOST,
-            database: DBNAME,
-            user: DBUSERNAME,
-            password: DBPASSWORD,
-        });
-        connection.connect((err) => {
-            if (err) {
-                console.error('error connecting: ' + err.stack);
-                return;
-            }
-
-            console.log('Connected as ID: ' + connection.threadId);
-        });
-        return connection;
+const connection = mysql.createConnection({
+    host: DBHOST,
+    database: DBNAME,
+    user: DBUSERNAME,
+    password: DBPASSWORD,
+});
+connection.connect((err) => {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        throw err;
     }
 
-    addUser(req, res) {
-        const connection = DB.getConnection();
-        connection.beginTransaction((err) => {
-            if (err) { throw err; }
-            connection.query('INSERT INTO users(username, name, password) VALUES(?,?,?);', ['roshanusername1211', 'roshan1', 'pass123'], (error, results, fields) => {
-                if (error) {
-                    return connection.rollback(() => {
-                        res.send({
-                            status: error.code,
-                        });
-                        throw error;
-                    });
-                }
-                connection.commit((err) => {
-                    if (err) {
-                        return connection.rollback(() => {
-                            res.send({
-                                status: error.code,
-                            });
-                            throw err;
-                        });
-                    }
-                    console.log = 'User ' + results.insertId + ' added';
-                    res.status(200).send({
-                        status: 'success',
-                    });
-                });
-            });
-        });
-    }
-}
+    console.log('Connected as ID: ' + connection.threadId);
+});
 
-const db = new DB();
-module.exports = db;
+module.exports = connection;
