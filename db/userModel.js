719257c69f55599946d07014da5f64c2834b90ca
Roshan Adhikari <nahsorad@gmail.com>
2019-08-29 16:40:45 +0545

    Add verifyUser function

--- db/userModel.js
+++ db/userModel.js
@@ -1,4 +1,5 @@
 /* eslint-disable consistent-return */
+const bcrypt = require('bcrypt');
 const db = require('./index.js');
 
 class UserModel {
@@ -21,7 +22,7 @@ class UserModel {
             name,
             password,
         } = req.body;
-        
+
         db.query('INSERT INTO users(username, name, password) VALUES(?,?,?);', [username, name, password], (error, results, fields) => {
             if (error) {
                 return db.rollback(() => {
@@ -45,6 +46,63 @@ class UserModel {
                     status: 'success',
                 });
             });
+        });
+    }
+
+    verifyUser(req, res) {
+        if (!req.body.username) {
+            res.status(400).send({
+                status: 'NO-USERNAME',
+            });
+        } else if (!req.body.password) {
+            res.status(400).send({
+                status: 'NO-PASSWORD',
+            });
+        }
+        const {
+            username,
+            password,
+        } = req.body;
+
+        db.query('SELECT password FROM users WHERE username=?;', [username], (error, results, fields) => {
+            if (error) {
+                return db.rollback(() => {
+                    res.send({
+                        status: error.code,
+                    });
+                    throw error;
+                });
+            }
+            console.log(results.length + ' user found!');
+            const len = results.length;
+            if (len > 0) {
+                // User exists
+                // Generate Salt for hashing (with 10 rounds) / ASYNC
+                bcrypt.genSalt(5, (err, salt) => {
+                    // Generate Hash for the password / ASYNC
+                    bcrypt.hash(password, salt, (err, hash) => {
+                        const retrievedHash = results[0].password;
+                        // Compare plain password with retrieved hash
+                        bcrypt.compare(password, retrievedHash, (err, result) => {
+                            if (result === true) {
+                                res.status(200).send({
+                                    status: 'success',
+                                });
+                            } else {
+                                // User exists but incorrect password
+                                res.status(401).send({
+                                    status: 'PASSWORD-MISMATCH',
+                                });
+                            }
+                        });
+                    });
+                });
+            } else {
+                // User doesn't exist
+                res.status(404).send({
+                    status: 'USERNAME-NOT-FOUND',
+                });
+            }
         });
     }
 }
