f8aecf3a0dfa5a6eca586e79575426b8cac02355
Roshan Adhikari <nahsorad@gmail.com>
2019-08-29 14:52:52 +0545

    Update utility.js for exporting DB connection info

--- utility.js
+++ utility.js
@@ -2,12 +2,17 @@ require('dotenv').config();
 
 // Environment variables
 const PORT = process.env.PORT || 3000;
-const { OMDB_KEY } = process.env;
-const { TMDB_KEY } = process.env;
-const { BASE_URL } = process.env;
-const { API_KEY_STRING } = process.env;
-const { QUERY_STRING } = process.env;
-const { PRIMARY_RELEASE_YEAR_STRING } = process.env;
+const {
+    OMDB_KEY,
+    TMDB_KEY,
+    BASE_URL,
+    API_KEY_STRING,
+    QUERY_STRING,
+    PRIMARY_RELEASE_YEAR_STRING,
+    DBHOST,
+    DBUSERNAME,
+    DBPASSWORD,
+} = process.env;
 
 module.exports = {
     PORT,
@@ -17,4 +22,7 @@ module.exports = {
     API_KEY_STRING,
     QUERY_STRING,
     PRIMARY_RELEASE_YEAR_STRING,
+    DBHOST,
+    DBUSERNAME,
+    DBPASSWORD,
 };
