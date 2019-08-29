a3d4ce08954aad55409bb3e41dbf299ad78e125c
Roshan Adhikari <nahsorad@gmail.com>
2019-08-29 19:49:26 +0545

    Add addMovieToWatchedList function

--- db/historyModel.js
+++ db/historyModel.js
@@ -79,6 +79,109 @@ class HistoryModel {
                 });
             });
     }
+
+    addMovieToWatchedList(req, res) {
+        if (!req.body.username) {
+            res.status(400).send({
+                status: 'NO-USERNAME',
+            });
+        } else if (!req.body.listType) {
+            res.status(400).send({
+                status: 'NO-LIST-TYPE',
+            });
+        } else if (!req.body.titleId) {
+            res.status(400).send({
+                status: 'NO-TITLE-ID',
+            });
+        } else if (!req.body.titleName) {
+            res.status(400).send({
+                status: 'NO-TITLE-NAME',
+            });
+        } else if (!req.body.titleOverview) {
+            res.status(400).send({
+                status: 'NO-TITLE-OVERVIEW',
+            });
+        } else if (!req.body.titleVoteCount) {
+            res.status(400).send({
+                status: 'NO-TITLE-VOTE-COUNT',
+            });
+        } else if (!req.body.titleVoteAverage) {
+            res.status(400).send({
+                status: 'NO-TITLE-VOTE-AVERAGE',
+            });
+        } else if (!req.body.titlePosterPath) {
+            res.status(400).send({
+                status: 'NO-TITLE-POSTER-PATH',
+            });
+        } else if (!req.body.titleType) {
+            res.status(400).send({
+                status: 'NO-TITLE-TYPE',
+            });
+        }
+
+        const {
+            listType,
+            titleId,
+            titleName,
+            titleOverview,
+            titleVoteCount,
+            titleVoteAverage,
+            titlePosterPath,
+            titleType,
+            username,
+        } = req.body;
+
+        db.query('INSERT INTO history(listType, titleId, titleName,titleOverview, titleVoteCount, titleVoteAverage,titlePosterPath, titleType, username) VALUES (?,?,?,?,?,?,?,?,?);',
+            [listType, titleId, titleName, titleOverview, titleVoteCount, titleVoteAverage, titlePosterPath, titleType, username], (error, results, fields) => {
+                if (error) {
+                    return db.rollback(() => {
+                        res.status(404).send({
+                            status: error.code,
+                        });
+                        throw error;
+                    });
+                }
+                db.commit((err) => {
+                    if (err) {
+                        return db.rollback(() => {
+                            res.send({
+                                status: err.code,
+                            });
+                            throw err;
+                        });
+                    }
+                    console.log = 'Movie ' + titleId + ' added to ' + username + '\'s ' + listType;
+
+                    // Added to watchedList, now check if movie is present in wishlist (then remove)
+                    db.query('SELECT * FROM history WHERE username=? AND listType=? AND titleId=? AND titleType=?;', [username, listType, titleId, titleType], (error, results, fields) => {
+                        if (error) {
+                            return db.rollback(() => {
+                                throw error;
+                            });
+                        }
+                        if (results.length > 0) {
+                            // Movie is in wishList..
+                            // so remove it from wishList
+                            db.query('DELETE FROM history WHERE listType=? AND titleId=? AND username=? AND titleType=?;',
+                                ['wishList', titleId, username, titleType],
+                                (error, results, fields) => {
+                                    if (error) {
+                                        res.status(404).send({
+                                            status: error.code,
+                                        });
+                                        throw error;
+                                    }
+                                    // Removed from wishList, and has been added to watchedList
+                                    // console.log('Deleted ' + results.affectedRows + ' rows from wishList');
+                                });
+                        }
+                        res.status(200).send({
+                            status: 'success',
+                        });
+                    });
+                });
+            });
+    }
 }
 
 const historyModel = new HistoryModel();
