47158324ffe3d92e2115c30bd6cead6ead3d4cdb
Roshan Adhikari <nahsorad@gmail.com>
2019-09-19 13:21:05 +0545

    Return 10 recent titles from getRecentTitles

--- db/historyModel.js
+++ db/historyModel.js
@@ -670,11 +670,11 @@ class HistoryModel {
                 const recentTitles = [];
                 let lowerLimit = 0;
                 switch (len) {
-                case len <= 5:
+                case len <= 10:
                     lowerLimit = 0;
                     break;
-                case len > 6:
-                    lowerLimit = len - 6;
+                case len > 10:
+                    lowerLimit = len - 10;
                     break;
                 default:
                     break;
